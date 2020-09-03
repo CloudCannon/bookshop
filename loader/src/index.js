ced607f3003b734d736d7b04cf956e8a8400bca9
Liam Bigelow <liam@cloudcannon.com>
2020-09-04 09:06:51 +1200

    WIP - support objects / repeats / selects / comments

--- loader/src/index.js
+++ loader/src/index.js
@@ -3,14 +3,35 @@ let toml = require('toml');
 let ejs = require('ejs');
 let changeCase = require("change-case");
 
+// TODO: BIG REFACTOR
+
 const storyTemplate = `
 const { jekyllEngine } = require("@bookshop/jekyll-engine");
 const { underscoreEngine } = require("@bookshop/underscore-engine");
 
 const component = props => {
   if (!props.framework) props.framework = "<%- frameworks[0] %>";
-  if (props.framework === "jekyll") return jekyllEngine.render("<%- component %>", props);
-  if (props.framework === "jst-ejs") return underscoreEngine.render("<%- component %>", props);
+  let consolidatedProps = {};
+  for (let [key, val] of Object.entries(props)) {
+    let splitKey = key.split('&&');
+    if (splitKey.length == 1) {
+      consolidatedProps[key] = val;
+      continue;
+    }
+    let [baseKey, innerKey] = splitKey;
+    consolidatedProps[baseKey] = consolidatedProps[baseKey] || {};
+    if (innerKey === "repeat-count") {
+      let baseObj = consolidatedProps[baseKey];
+      consolidatedProps[baseKey] = [];
+      for (var i = 0; i < val; i++) {
+        consolidatedProps[baseKey].push(baseObj);
+      }
+      continue;
+    }
+    consolidatedProps[baseKey][innerKey] = val;
+  }
+  if (props.framework === "jekyll") return jekyllEngine.render("<%- component %>", consolidatedProps);
+  if (props.framework === "jst-ejs") return underscoreEngine.render("<%- component %>", consolidatedProps);
 }
 
 export default {
@@ -33,7 +54,24 @@ export const <%= stories[i].name %> = component.bind({});
  */
 module.exports = function(source) {
   //todo: report meaningful errors
-  let data = toml.parse(source);
+  let splitLines = source.split('\n'), outputLines = [];
+  for (line of splitLines) {
+    outputLines.push(line);
+    if (/^[a-z0-9\s]+=.*?#.+?$/i.test(line)) {
+      let comment = line.match(/^([^"]+|[^"]*?"[^"]*?"[^"]*?)#([^#]+)$/i);
+      let variableName = line.match(/^\s*?([a-z0-9-_]+)\s?=/i);
+      if (!variableName || !comment) continue;
+
+      outputLines.push(`${variableName[1]}--doc = "${comment[2].trim()}"`);
+    } else if (/^\s*?\[.*?#.+?$/i.test(line)) {
+      let comment = line.match(/^([^"]+|[^"]*?"[^"]*?"[^"]*?)#([^#]+)$/i);
+      if (!comment) continue;
+      outputLines.push(`--doc = "${comment[2].trim()}"`);
+    }
+  }
+  let rejoinedToml = outputLines.join('\n');
+
+  let data = toml.parse(rejoinedToml);
 
   const frameworks = findFrameworkFiles(this);
   const hasMultipleFrameworks = frameworks.length > 1;
@@ -47,7 +85,7 @@ module.exports = function(source) {
     }
   }
 
-  const defaultArgTypes = processProps(data.defaults);
+  const defaultArgTypes = propsToArgTypes(data.defaults);
   if (hasMultipleFrameworks) {
     defaultArgTypes["framework"] = {
       control: {
@@ -58,7 +96,7 @@ module.exports = function(source) {
   }
 
   let defaults = {
-    args: JSON.stringify(data.defaults),
+    args: JSON.stringify(propsToArgs(data.defaults)),
     argTypes: JSON.stringify(defaultArgTypes)
   }
   delete data.defaults;
@@ -67,8 +105,8 @@ module.exports = function(source) {
   for (let [story, props] of Object.entries(data)) {
     stories.push({
       name: changeCase.camelCase(story),
-      args: JSON.stringify(props),
-      argTypes: JSON.stringify(processProps(props))
+      args: JSON.stringify(propsToArgs(props)),
+      argTypes: JSON.stringify(propsToArgTypes(props))
     })
   }
 
@@ -98,7 +136,6 @@ const findFrameworkFiles = context => {
     }
   }
 
-  console.log(`\nFound ${componentName} files: ${fileList.join(', ')}`);
   return fileList;
 }
 
@@ -122,71 +159,213 @@ const getTitleFromPath = context => {
 /**
  * Turn TOML fields into @storybook/controls argTypes
  */
-const processProps = props => {
+const propsToArgTypes = props => {
   if (!props) return {};
   let argTypes = {};
   Object.entries(props).map(([key, val]) => {
-    argTypes[key] = {
-      control: {
-        type: typeFromVal(val).name
+    if (/--doc/.test(key)) return;
+    let propData = typeFromVal(key, val);
+    for (let item of propData) {
+      argTypes[item.key] = {
+        control: {
+          type: item.name,
+          options: item.options || null
+        },
+        name: item.label || item.key,
+        description: findDocForProp(item.rawKey || item.key, item.rawOptions, props),
+        defaultValue: ''
       }
     }
   });
   return argTypes;
 };
 
-const typeFromVal = val => {
+/**
+ * Turn TOML fields into @storybook/controls args
+ */
+const propsToArgs = props => {
+  if (!props) return {};
+  let args = {};
+  Object.entries(props).map(([key, val]) => {
+    if (/--doc/.test(key)) return;
+    let propData = typeFromVal(key, val);
+    for (let item of propData) {
+      args[item.key] = item.value
+    }
+  });
+  return args;
+};
+
+const typeFromVal = (key, val) => {
   if (typeof val === 'undefined') {
-    return {
-      name:'text',
-      value: `""`
-    };
+    return [{
+          name:'text',
+          value: val,
+          key: key
+        }];
   }
   if (typeof val === 'string') {
-    if (val[0] === '#' && val.length > 3) {
-      return {
-        name:'color',
-        value: `"${val}"`
-      };
-    }
-    if (val[0] === '{' || val[0] === '[') {
-      return {
-        name:'object',
-        value: `${val}`
-      };
-    }
-    return {
-      name:'text',
-      value: `"${val}"`
-    };
+    return determineStringVal(key, val);
   }
   if (typeof val === 'boolean') {
-    return {
-      name:'boolean',
-      value: val
-    };
+    return [{
+          name:'boolean',
+          value: val,
+          key: key
+        }];
   }
   if (typeof val === 'number') {
-    return {
-      name:'number',
-      value: val
-    };
+    return [{
+          name:'number',
+          value: val,
+          key: key
+        }];
   }
   if (Array.isArray(val)) {
-    return {
-      name:'array',
-      value: JSON.stringify(val)
-    };
+    return [{
+          name:'array',
+          value: val,
+          key: key
+        }];
   }
-  // todo: Add support for selects in Storybook's controls
-  // if (Object.keys(val).length) {
-  //   return {
-  //     name:'select',
-  //     value: JSON.stringify(val)
-  //   };
-  // }
-  return {
-    name:'text',
-    value: JSON.stringify(val)
-  };
+  if (Object.keys(val).length) {
+    return determineObjectType(key, val);
+  }
+  return [{
+      name:'text',
+      value: val,
+      key: key
+    }];
+}
+
+const determineObjectType = (key, val) => {
+  let rawKey = key;
+  let splitKey = key.split('--'), enumType;
+  if (splitKey.length == 1) splitKey[1] = 'o';
+
+  [key, enumType] = splitKey;
+
+  if (/^(select|multi-select|radio|inline-radio|check|inline-check)$/.test(enumType)) {
+    return [{
+        name:enumType,
+        value: val[Object.keys(val).find(v => v !== '--doc')],
+        options: capitalizeKeys(val),
+        rawOptions: val,
+        key: key,
+        rawKey: rawKey
+      }];
+  }
+
+  if (enumType == 'repeat' || enumType == 'o') {
+    let repeatControls = [];
+
+    repeatControls.push({
+      name: 'radio',
+      key: `${key}&&object-info-field`,
+      rawKey: `${rawKey}&&object-info-field`,
+      label: `${key}`,
+      options: {}
+    });
+
+    for (let [controlKey, controlVal] of Object.entries(val)) {
+      if (/--doc/.test(controlKey)) continue;
+      let innerKeys = typeFromVal(controlKey, controlVal);
+      for (let ik of innerKeys) {
+        repeatControls.push({
+          ...ik,
+          key: `${key}&&${ik['key']}`,
+          rawKey: `${rawKey}&&${ik['key']}`,
+          label: `${enumType == 'repeat' ? '🔁' : '↪️'} ${key}${enumType == 'repeat' ? '[n]' : ''}.${ik['key']}`
+        });
+      }
+    }
+    if (enumType == 'repeat') {
+      repeatControls.push({
+        name: 'range',
+        value: 3,
+        key: `${key}&&repeat-count`,
+        rawKey: `${rawKey}&&repeat-count`,
+        label: `Number of ${key}`
+      });
+    }
+    return repeatControls;
+  }
+
+  return [{
+      name:'object',
+      value: val,
+      key: key
+    }];
+}
+
+/**
+ * Try and guess what input type the string represents.
+ */
+const determineStringVal = (key, val) => {
+  if (/#([0-9a-f]{3}|[0-9a-f]{6}|[0-9a-f]{8})/i.test(val)) {
+    return [{
+          name:'color',
+          value: val,
+          key: key
+        }];
+  }
+  if (val[0] === '{' || val[0] === '[') {
+    return [{
+          name:'object',
+          value: val,
+          key: key
+        }];
+  }
+  return [{
+      name:'text',
+      value: val,
+      key: key
+    }];
+}
+
+/**
+ * Capitalize keys in an object.
+ * Use this when padding a set of options to storybook,
+ * i.e. for a select box. Makes things prettier.
+ */
+const capitalizeKeys = o => {
+  let output = {};
+  for (let [key, val] of Object.entries(o)) {
+    if (/--doc/.test(key)) continue;
+    output[changeCase.capitalCase(key)] = val;
+  }
+  return output;
+}
+
+/**
+ * When processing the TOML we turn comments into fields.
+ * This function finds any of those fields if they exist.
+ */
+const findDocForProp = (key, options, props) => {
+  key = key.replace(`&&object-info-field`, '&&');
+  let docVal;
+
+  if (options && Object.keys(options).length) {
+    docVal = options[`--doc`]
+    if (docVal) {
+      return docVal;
+    }
+  }
+
+  docVal = drillForProp(`${key}--doc`, props);
+  if (docVal) {
+    return docVal;
+  }
+  return "";
+}
+
+/**
+ * Search for a prop deep within an object
+ * @param  {String} key   Path to key e.g. "line.text.item"
+ * @param  {Object} props Object to search
+ */
+function drillForProp(key, props) {
+  return key.split('&&').reduce(function(prev, curr) {
+    return prev ? prev[curr] : null
+  }, props || self)
 }
