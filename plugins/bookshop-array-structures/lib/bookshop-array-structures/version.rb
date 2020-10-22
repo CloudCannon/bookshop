fcbe28651942a02b2266a8ed9d4e4fd9a966cdd3
bigelowcc <liam@cloudcannon.com>
2020-10-22 18:05:35 +1300

    Amend component tag to allow variable interpolation

--- plugins/bookshop-array-structures/lib/bookshop-array-structures/version.rb
+++ plugins/bookshop-array-structures/lib/bookshop-array-structures/version.rb
@@ -1,5 +1,5 @@
 module Bookshop
   module Arraystructures
-    VERSION = "1.4.4"
+    VERSION = "1.4.5"
   end
 end
--- plugins/jekyll-bookshop/lib/jekyll-bookshop.rb
+++ plugins/jekyll-bookshop/lib/jekyll-bookshop.rb
@@ -3,18 +3,13 @@ require 'pathname'
 
 module JekyllBookshop
   class Tag < Jekyll::Tags::IncludeTag
-    def initialize(tag_name, markup, tokens)
-      cpath = markup.strip.partition(" ").first
-      cname = cpath.strip.split("/").last
-      tag = markup.strip.partition(" ").last
-      markup = "#{cpath}/#{cname}.jekyll.html #{tag}"
-      super
-    end
 
+    # Look for includes in the built bookshop directory
     def tag_includes_dirs(context)
       Array(Pathname.new(context['site']['bookshop_path'] + '/components').cleanpath.to_s).freeze
     end
 
+    # Support the bind syntax, spreading an object into params
     def parse_params(context)
       params = super
 
@@ -28,9 +23,39 @@ module JekyllBookshop
 
       params
     end
+
+    # Map component names to the .jekyll.html files found in bookshop
+    def render(context)
+      site = context.registers[:site]
+
+      file = render_variable(context) || @file
+      cname = file.strip.split("/").last
+      file = "#{file}/#{cname}.jekyll.html"
+      validate_file_name(file)
+
+      path = locate_include_file(context, file, site.safe)
+      return unless path
+
+      add_include_to_dependency(site, path, context)
+
+      partial = load_cached_partial(path, context)
+
+      context.stack do
+        context["include"] = parse_params(context) if @params
+        begin
+          partial.render!(context)
+        rescue Liquid::Error => e
+          e.template_name = path
+          e.markup_context = "included " if e.markup_context.nil?
+          raise e
+        end
+      end
+    end
   end
 
   class Styles
+
+    # Add the paths to find bookshop's styles
     def self.open_bookshop(site)
       bookshop_path = site.theme.root + '/_bookshop'
       site.config['bookshop_path'] = Pathname.new(bookshop_path).cleanpath.to_s
