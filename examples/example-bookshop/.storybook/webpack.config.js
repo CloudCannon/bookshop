54f1782ebc31d9a5d6a2195902d74ef8a791e611
Tate <tate@cloudcannon.com>
2020-11-06 13:41:47 +1300

    Refactor example webpack config to use new engine options

--- examples/example-bookshop/.storybook/webpack.config.js
+++ examples/example-bookshop/.storybook/webpack.config.js
@@ -1,90 +1,92 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
-const createCompiler = require('@storybook/addon-docs/mdx-compiler-plugin');
-const {
-    rewriteIncludes
-} = require('@bookshop/jekyll-engine');
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const createCompiler = require("@storybook/addon-docs/mdx-compiler-plugin");
+const { rewriteIncludes } = require("@bookshop/jekyll-engine");
+const { svelteEngine } = require("@bookshop/svelte-engine");
+const { jekyllEngine } = require("@bookshop/jekyll-engine");
+const { underscoreEngine } = require("@bookshop/underscore-engine");
+const projectRoot = path.resolve(__dirname, "../");
 
-const projectRoot = path.resolve(__dirname, '../');
+module.exports = async ({ config, mode }) => {
+  config.module.rules.push({
+    test: /\.stories\.to?ml?$/,
+    use: [
+      {
+        loader: "@bookshop/loader",
+        options: {
+          mdxCompiler: createCompiler({}),
+          engines: [
+            { engine: svelteEngine, extension: "svelte" },
+            { engine: jekyllEngine, extension: "jekyll.html" },
+            { engine: underscoreEngine, extension: "jst.ejs" },
+          ],
+        },
+      },
+    ],
+  });
 
-module.exports = async({
-    config,
-    mode
-}) => {
-    config.module.rules.push({
-        test: /\.stories\.to?ml?$/,
-        use: [
-            {
-                loader: '@bookshop/loader',
-                options: {
-                    mdxCompiler: createCompiler({})
-                }
-            }
-        ]
-    });
+  config.module.rules.push({
+    test: /\.scss$/,
+    use: [
+      "style-loader",
+      {
+        loader: "css-loader",
+        options: {
+          sourceMap: false,
+        },
+      },
+      {
+        loader: "resolve-url-loader",
+        options: {
+          debug: false,
+          root: projectRoot,
+        },
+      },
+      {
+        loader: "sass-loader",
+        options: {
+          implementation: require("node-sass"),
+          sassOptions: {
+            importer: require("stretcher-importer"),
+          },
+        },
+      },
+      "import-glob",
+      "stretcher-loader",
+    ],
+    include: [projectRoot],
+  });
 
-    config.module.rules.push({
-        test: /\.scss$/,
-        use: [
-            'style-loader', {
-                loader: 'css-loader',
-                options: {
-                    sourceMap: false
-                }
-            }, {
-                loader: 'resolve-url-loader',
-                options: {
-                    debug: false,
-                    root: projectRoot,
-                }
-            }, {
-                loader: 'sass-loader',
-                options: {
-                    implementation: require("node-sass"),
-                    sassOptions: {
-                        importer: require('stretcher-importer')
-                    }
-                }
-            },
-            'import-glob',
-            'stretcher-loader'
-        ],
-        include: [
-            projectRoot,
-        ],
-    });
+  config.module.rules.push({
+    test: /\.svelte$/,
+    use: ["svelte-loader"],
+    include: [projectRoot],
+  });
 
-    config.module.rules.push({
-        test: /\.svelte$/,
-        use: [
-            'svelte-loader'
-        ],
-        include: [
-            projectRoot,
-        ],
-    });
+  config.plugins.push(
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(projectRoot, "components/**/*"),
+          context: path.resolve(projectRoot, "components"),
+          to: "./components",
+          globOptions: {
+            ignore: ["*.stories.*"],
+          },
+          transform(content, path) {
+            return rewriteIncludes(content, path);
+          },
+        },
+        {
+          from: path.resolve(projectRoot, "assets"),
+          to: "./assets",
+          globOptions: {
+            ignore: ["*.scss"],
+          },
+        },
+      ],
+    })
+  );
 
-    config.plugins.push(
-        new CopyPlugin({
-            patterns: [{
-                from: path.resolve(projectRoot, 'components/**/*'),
-                context: path.resolve(projectRoot, 'components'),
-                to: './components',
-                globOptions: {
-                    ignore: ['*.stories.*'],
-                },
-                transform(content, path) {
-                    return rewriteIncludes(content, path);
-                }
-            }, {
-                from: path.resolve(projectRoot, 'assets'),
-                to: './assets',
-                globOptions: {
-                    ignore: ['*.scss'],
-                }
-            }]
-        })
-    );
-
-    return config;
+  return config;
 };
