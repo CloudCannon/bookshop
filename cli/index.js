1440fe9a3cd1f5b5d8d77b2e2651766944ce7fd4
Robbie Cook <robbieck7@gmail.com>
2020-12-11 12:56:31 +1300

    Remove temp dir no matter whatÏ

--- cli/index.js
+++ cli/index.js
@@ -10,19 +10,22 @@ const path = require("path");
  * A CLI package to help init a bookshop package
  */
 async function run() {
-  const cli = meow(`
+  const cli = meow(
+    `
     Usage
       $ npx bookshop --init <output-dir>
  
     Examples
       $ npx bookshop --init .
-  `, {
-    flags: {
-      init: {
-        isRequired: true
-      }
+  `,
+    {
+      flags: {
+        init: {
+          isRequired: true,
+        },
+      },
     }
-  });
+  );
 
   const spinner = ora("Downloading bookshop...").start();
 
@@ -39,11 +42,10 @@ async function run() {
 
   // Copy the example project into user's specified directory
   try {
-  execSync(
-    `cp -r ${__dirname}/temp/examples/example-bookshop/ ${output}`
-  );
+    execSync(`cp -r ${__dirname}/temp/examples/example-bookshop/ ${output}`);
   } catch (ex) {
     console.error(`Command failed! ${ex}`);
+    execSync(`rm -rf ${__dirname}/temp`);
     process.exit(1);
   }
 
