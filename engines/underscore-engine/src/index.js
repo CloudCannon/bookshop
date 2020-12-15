5365801684f667ae53b9832ac276f5c210bc1635
Robbie Cook <robbieck7@gmail.com>
2020-12-15 16:02:34 +1300

    Finished engine rework

--- engines/underscore-engine/src/index.js
+++ engines/underscore-engine/src/index.js
@@ -1,5 +1,6 @@
 const _ = require("underscore");
 const ejs = require("ejs");
+console.log('hello');
 
 const underscoreEngine = {
   render: (name, props, options) => {
@@ -10,39 +11,6 @@ const underscoreEngine = {
     request.send(null);
     options.renderRoot.innerHTML = ejs.render(request.responseText);
   }
-}
-
-var JSTHandler = {
-  get: (o, name) => {
-    let request = new XMLHttpRequest();
-    let cpath = name.split('/');
-    let cname = cpath[cpath.length - 1];
-    request.open('GET', `/components/${name}/${cname}.ejs`, false);  // `false` makes the request synchronous
-    request.send(null);
-    return _.template(request.responseText);
-  },
-};
-
-window.JST = new Proxy({}, JSTHandler);
-
-window.ADDMODS = (classname, mods) => {
-    let base = classname.split(' ')[0];
-    for (let [mod, val] of Object.entries(mods)) {
-        if (val) {
-            classname = `${classname} ${base}--${mod}`;
-        }
-    }
-    return classname;
-}
-
-window.ADDSTATES = (states) => {
-    let classname = '';
-    for (let [state, val] of Object.entries(states)) {
-        if (val) {
-            classname = `${classname} is-${state}`;
-        }
-    }
-    return classname;
 }
 
 module.exports = {
