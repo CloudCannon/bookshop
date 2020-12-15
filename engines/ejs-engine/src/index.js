df1fc2e43761128c200f8a56ec7c0a3804af70cd
Robbie Cook <robbieck7@gmail.com>
2020-12-16 11:15:33 +1300

    Passing ejs props to story

--- engines/ejs-engine/src/index.js
+++ engines/ejs-engine/src/index.js
@@ -8,7 +8,7 @@ const engine = {
     let cname = cpath[cpath.length - 1];
     request.open('GET', `/components/${name}/${cname}.ejs`, false);  // `false` makes the request synchronous
     request.send(null);
-    options.renderRoot.innerHTML = ejs.render(request.responseText);
+    options.renderRoot.innerHTML = ejs.render(request.responseText, { props });
   }
 }
 
