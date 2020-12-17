56c4ced235cf9b7725b0df4b6c4396ef2e1c674f
Robbie Cook <robbieck7@gmail.com>
2020-12-17 14:42:35 +1300

    Supporting ejs includes in storybook

--- engines/ejs-engine/src/index.js
+++ engines/ejs-engine/src/index.js
@@ -1,16 +1,39 @@
 const ejs = require("./ejs");
 
-const engine = {
-  render: (name, props, options) => {
-    let request = new XMLHttpRequest();
-    let cpath = name.split('/');
-    let cname = cpath[cpath.length - 1];
-    request.open('GET', `/components/${name}/${cname}.ejs`, false);  // `false` makes the request synchronous
-    request.send(null);
-    options.renderRoot.innerHTML = ejs.render(request.responseText, { props });
-  }
+function fetchComponent(name) {
+  let request = new XMLHttpRequest();
+  let cpath = name.split("/");
+  let cname = cpath[cpath.length - 1];
+  request.open("GET", `/components/${name}/${cname}.ejs`, false); // `false` makes the request synchronous
+  request.send(null);
+  return request.responseText;
 }
 
+const engine = {
+  render: (name, props, options) => {
+    options.renderRoot.innerHTML = ejs.render(fetchComponent(name), { props });
+  },
+};
+
+/**
+ *
+ * Storybook handling for `include`
+ *
+ * @param {string} filename
+ * @param {Record<string, any>} props
+ *
+ * @return {string} EJS component string
+ */
+window.include = (filename, props) => {
+  console.log(props);
+  // https://stackoverflow.com/questions/1661197/what-characters-are-valid-for-javascript-variable-names
+  const componentName = filename
+    .match(/([a-zA-Z_$][0-9a-zA-Z_$]*)\.ejs/g)[0]
+    .replace(".ejs", "");
+  const ejsComponent = fetchComponent(componentName);
+  return ejs.render(ejsComponent, props);
+};
+
 module.exports = {
-    ejsEngine: engine
-}
+  ejsEngine: engine,
+};
