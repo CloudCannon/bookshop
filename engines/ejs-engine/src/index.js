48e5e56225a26a39bef69cde016e284a7347d684
Robbie Cook <robbieck7@gmail.com>
2021-01-06 08:44:12 +1300

    Updating syntax as per request from app team

--- engines/ejs-engine/src/index.js
+++ engines/ejs-engine/src/index.js
@@ -12,7 +12,7 @@ const engine = {
     const nameParts = name.split("/");
     const lastPart = nameParts[nameParts.length - 1];
     options.renderRoot.innerHTML = ejs.render(
-      `<% include = window.include %>${fetchComponent(
+      `${fetchComponent(
         `components/${name}/${lastPart}.ejs`
       )}`,
       { props }
@@ -22,20 +22,19 @@ const engine = {
 
 /**
  *
- * Storybook handling for `include`
+ * Storybook handling for including other components
  *
  * @param {string} filename
  * @param {Record<string, any>} props
  *
  * @return {string} EJS component string
  */
-window.include = (path, props) => {
+window.component = (path, props) => {
   const parts = path.split("/");
   const lastPart = parts[parts.length - 1].replace(".ejs", "");
 
   const ejsComponent = fetchComponent(
-    `${path.toLowerCase()}`
-    // path.replace('Components', 'components')
+    `components/${path.toLowerCase()}`
   );
   return ejs.render(ejsComponent, { props });
 };
