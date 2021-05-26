183cd213ec62ef570ac63d1dc195bbce2dcb52a5
Liam Bigelow <liam@cloudcannon.com>
2021-05-27 11:41:52 +1200

    Updating renderer to handle bookshop keys nested inside objects

--- renderer/lib/polymorph.svelte
+++ renderer/lib/polymorph.svelte
@@ -83,6 +83,10 @@
         if (!obj) return {};
         if (obj.defaults && typeof obj.defaults === 'object') return processOldProps(obj.defaults);
         obj = obj.props ?? {};
+        return processPropObject(obj);
+    }
+
+    const processPropObject = (obj) => {
         for (const key of Object.keys(obj)) {
             if (obj[key]?.select && Array.isArray(obj[key].select)) {
                 obj[key] = obj[key].select[0]
@@ -90,6 +94,18 @@
 
             if (obj[key]?.preview && Array.isArray(obj[key].preview)) {
                 obj[key] = obj[key].preview[0]
+            }
+
+            if (obj[key]?.default) {
+                obj[key] = obj[key].default
+            }
+
+            if (Array.isArray(obj[key])) {
+                obj[key].forEach((arr_obj, index) => {
+                    obj[key][index] = processPropObject(arr_obj);
+                })
+            } else if (typeof obj[key] === "object") {
+                obj[key] = processPropObject(obj[key]);
             }
         }
         return obj;
