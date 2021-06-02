2b3df9de96320e457451e4c073d33ac21a5ae63b
Liam Bigelow <liam@cloudcannon.com>
2021-06-02 14:34:15 +1200

    Fixing plugin bookshop location clash

--- plugins/jekyll-bookshop/lib/jekyll-bookshop.rb
+++ plugins/jekyll-bookshop/lib/jekyll-bookshop.rb
@@ -60,7 +60,7 @@ module JekyllBookshop
       site = context.registers[:site]
       
       bookshop_scss_files = []
-      site.config['bookshop_locations']&.each do |location|
+      site.config['bookshop_base_locations']&.each do |location|
         components_loc = Pathname.new(location + "/").cleanpath.to_s
         scss_files = Dir.glob(components_loc + "/**/*.scss")&.collect do |scss_file|
           scss_file.sub!(components_loc+"/", '').sub!(".scss", '')
@@ -88,25 +88,25 @@ module JekyllBookshop
 
     # Add the paths to find bookshop's styles
     def self.open_bookshop(site)
-      base_bookshop_locations = site.config['bookshop_locations']&.collect do |location|
+      bookshop_base_locations = site.config['bookshop_locations']&.collect do |location|
         Pathname.new("#{site.source}/#{location}/").cleanpath.to_s
       end
-      base_bookshop_locations = base_bookshop_locations.select do |location|
+      bookshop_base_locations = bookshop_base_locations.select do |location|
         Dir.exist?(location)
       end
-      bookshop_component_locations = base_bookshop_locations&.collect do |location|
+      bookshop_component_locations = bookshop_base_locations&.collect do |location|
         Pathname.new("#{location}/components/").cleanpath.to_s
       end
 
       site.config['watch_dirs'] ||= [] # Paired with CloudCannon/jekyll-watch
-      site.config['watch_dirs'].push(*base_bookshop_locations);
+      site.config['watch_dirs'].push(*bookshop_base_locations);
 
       site.config['sass'] ||= {}
       site.config['sass']['load_paths'] ||= []
-      site.config['sass']['load_paths'].push(*base_bookshop_locations)
+      site.config['sass']['load_paths'].push(*bookshop_base_locations)
 
-      site.config['bookshop_locations'] = []
-      site.config['bookshop_locations'].push(*base_bookshop_locations)
+      site.config['bookshop_base_locations'] ||= []
+      site.config['bookshop_base_locations'].push(*bookshop_base_locations)
 
       site.config['bookshop_component_locations'] ||= []
       site.config['bookshop_component_locations'].push(*bookshop_component_locations)
