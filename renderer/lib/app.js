e91f59f505f5e0b9d65140221b69a250e8eebe45
Liam Bigelow <liam@cloudcannon.com>
2021-06-09 17:51:10 +1200

    MVP live CloudCannon renderer

--- renderer/lib/app.js
+++ renderer/lib/app.js
@@ -1,6 +1,7 @@
 import bookshopComponents from `components`;
 import bookshopStyles from `components.bookshop_scss`;
 import Polymorph from './polymorph.svelte';
+import LiveComponent from './liveComponent.svelte';
 
 console.log('Bookshop polymorph initialising.');
 
@@ -25,30 +26,67 @@ const setupBookshopPolymorph = () => {
     
     const rootEl = document.querySelector('html');
     rootEl.setAttribute('data-bookshop-hmr', 'true');
-
+    
     const head = document.getElementsByTagName('head')[0];
     const style = document.createElement('style');
     style.dataset.bookshopStyle = true;
     style.innerHTML = bookshopStyles + coreStyles;
     head.appendChild(style);
     window.polymorphStyle = style;
-
-    const polymorphRenderTarget = document.querySelector('[data-bookshop-polymorph]');
-    polymorphRenderTarget.innerHTML = "";
-    window.polymorphRenderer = new Polymorph({
-        target: polymorphRenderTarget,
-        props: {
-            components: components
+    
+    const polymorphRenderTargets = document.querySelectorAll('[data-bookshop-polymorph]');
+    polymorphRenderTargets.forEach(polymorphRenderTarget => {
+        polymorphRenderTarget.innerHTML = "";
+        new Polymorph({
+            target: polymorphRenderTarget,
+            props: {
+                components: components
+            }
+        });
+    })
+    
+    if (window.inEditorMode || true) {
+        window.liveComponents = window.liveComponents || [];
+        window.CloudCannon = {
+            trigger: function (eventName, frontMatter) {
+                window.liveComponents.forEach(liveComponent => {
+                    data = liveComponent.propSource.split('.').reduce((o,i)=>o[i], frontMatter)
+                    liveComponent.component.$$set({
+                        props: frontMatter
+                    })
+                });
+                console.log(`Pushing a ${eventName}`);
+            }
         }
-    });
+        window.top.CloudCannon = window.CloudCannon;
 
+        const liveRenderTargets = document.querySelectorAll('[data-bookshop-live]');
+        liveRenderTargets.forEach(target => {
+            const originalHTML = target.innerHTML;
+            const componentName = target.dataset.bookshopLive;
+            const componentPropSource = target.dataset.bookshopProps;
+            target.innerHTML = "";
+            window.liveComponents.push({
+                propSource: componentPropSource?.replace(/^page\./, ''),
+                component: new LiveComponent({
+                    target: target,
+                    props: {
+                        components: components,
+                        originalHTML: originalHTML,
+                        selectedComponent: componentName
+                    }
+                })
+            });
+        });
+    }
+    
     if (BOOKSHOP_HMR_AVAILABLE) {
         const socket = new WebSocket(`ws://localhost:${BOOKSHOP_HMR_PORT}/`);
-
+        
         socket.addEventListener('open', function (event) {
             socket.send('Hello Server!');
         });
-
+        
         socket.addEventListener('message', function (event) {
             if (event.data === "new-components") {
                 document.querySelectorAll('[data-bookshop-reload]').forEach(el => {
@@ -69,7 +107,7 @@ const loadNewPolymorphComponents = () => {
         const componentName = filename.split('components/')[1]?.replace(/\/[^\/]+$/, '');
         let componentType = filename.match(/\/[^\/\.]+([^\/]+)$/)?.[1];
         componentType = componentMap[componentType] ?? 'other';
-
+        
         if (componentName) {
             if (!components[componentName]) {
                 components[componentName] = {};
@@ -77,13 +115,13 @@ const loadNewPolymorphComponents = () => {
             components[componentName][componentType] = source;
         }
     });
-
+    
     if (window.polymorphRenderer) {
         window.polymorphRenderer.$$set({
             components: components
         });
     }
-
+    
     if (window.polymorphStyle) {
         window.polymorphStyle.innerHTML = bookshopStyles + coreStyles;
     }
---
+++ renderer/lib/liveComponent.svelte
@@ -0,0 +1,44 @@
+<script>
+    import {onMount} from 'svelte';
+    import getJekyllEngine from './jekyll/engine';
+
+    export let components = {};
+    export let selectedComponent = 'nothing';
+    export let originalHTML = '';
+    export let props = null;
+
+    let selectedFramework = 'jekyll';
+    let outputHTML = '';                    // Static rendered component
+
+    // TODO: Decouple this from the jekyll engine again
+    const getSourceFn = (componentName, type) => {
+        componentName = componentName.replace(/\/$/, '');
+        return hydratedComponents[componentName]?.frameworks?.[type] ?? '';
+    }
+    const jekyllEngine = getJekyllEngine(getSourceFn);
+
+    // Pre-parse all of our TOML files
+    const hydrate = (ogComponents) => {
+        const o = {};
+        Object.entries(ogComponents).forEach(([key, component]) => {
+            o[key] = {
+                frameworks: {
+                    ...component
+                }
+            }
+        });
+        return o;
+    }
+    let hydratedComponents = {};
+    $: {
+        hydratedComponents = hydrate(components);
+        console.log(`Components Updated.`);
+    }
+
+    const render = async (component, props) => {
+        outputHTML = await jekyllEngine(getSourceFn(component, 'jekyll'), props);
+    };
+    $: if (hydratedComponents && props) render(selectedComponent, props);
+</script>
+
+{@html props ? outputHTML : originalHTML}
