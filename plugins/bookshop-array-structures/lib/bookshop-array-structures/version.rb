b3027cb4842e75270219a7ad2650afe6d29fd90c
Liam Bigelow <liam@cloudcannon.com>
2021-06-13 16:30:04 +1200

    Plugins alpha.10

--- plugins/bookshop-array-structures/lib/bookshop-array-structures/version.rb
+++ plugins/bookshop-array-structures/lib/bookshop-array-structures/version.rb
@@ -1,5 +1,5 @@
 module Bookshop
   module Arraystructures
-    VERSION = "2.0.0.pre.alpha.9"
+    VERSION = "2.0.0.pre.alpha.10"
   end
 end
--- plugins/jekyll-bookshop/lib/jekyll-bookshop/version.rb
+++ plugins/jekyll-bookshop/lib/jekyll-bookshop/version.rb
@@ -1,3 +1,3 @@
 module JekyllBookshop
-  VERSION = "2.0.0.pre.alpha.9"
+  VERSION = "2.0.0.pre.alpha.10"
 end
