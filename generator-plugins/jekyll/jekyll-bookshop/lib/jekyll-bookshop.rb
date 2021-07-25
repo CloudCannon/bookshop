bac831bf2a0cda6202b14553aa0735c03a7db9f6
Liam Bigelow <liam@cloudcannon.com>
2021-07-25 18:20:44 +1200

    Removing BEM filters from core bookshop

--- generator-plugins/jekyll/jekyll-bookshop/lib/jekyll-bookshop.rb
+++ generator-plugins/jekyll/jekyll-bookshop/lib/jekyll-bookshop.rb
@@ -7,13 +7,10 @@ require "dry/inflector"
 require_relative "jekyll-bookshop/tags/bookshop-tag"
 require_relative "jekyll-bookshop/tags/style-tag"
 require_relative "jekyll-bookshop/init-styles"
-require_relative "jekyll-bookshop/filters"
 require_relative "jekyll-bookshop/site-data"
 
 Liquid::Template.register_tag("bookshop", JekyllBookshop::Tag)
 Liquid::Template.register_tag("bookshop_scss", JekyllBookshop::StyleTag)
-
-Liquid::Template.register_filter(JekyllBookshop::Filters)
 
 Jekyll::Hooks.register :site, :after_init do |site|
   JekyllBookshop::Styles.open_bookshop(site)
