db3eac1983e18ebb72b82bedd4fbc157421599df
Liam Bigelow <liam@cloudcannon.com>
2021-08-03 14:07:25 +1200

    Add live editing initialization to cloudcannon-jekyll-bookshop

--- generator-plugins/jekyll/cloudcannon-jekyll-bookshop/lib/cloudcannon-jekyll-bookshop.rb
+++ generator-plugins/jekyll/cloudcannon-jekyll-bookshop/lib/cloudcannon-jekyll-bookshop.rb
@@ -3,6 +3,9 @@
 require "jekyll"
 require_relative "./cloudcannon-jekyll-bookshop/output-site-data"
 require_relative "./cloudcannon-jekyll-bookshop/structures"
+require_relative "./cloudcannon-jekyll-bookshop/tags/live-tag"
+
+Liquid::Template.register_tag("bookshop_live", CloudCannonJekyllBookshop::LiveTag)
 
 Jekyll::Hooks.register :site, :after_init do |site|
   CloudCannonJekyllBookshop::Structures.build_structures(site)
---
+++ generator-plugins/jekyll/cloudcannon-jekyll-bookshop/lib/cloudcannon-jekyll-bookshop/tags/live-tag.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module CloudCannonJekyllBookshop
+  class LiveTag < Liquid::Tag
+    def initialize(tag_name, markup, tokens)
+      super
+      @script = markup.strip      
+    end
+
+    def render(context)
+      site = context.registers[:site]
+
+      "<script>
+window.addEventListener('load', function() {
+  if (window.inEditorMode) {
+    const head = document.querySelector('head');
+    const script = document.createElement('script');
+    script.src = `/#{@script}`;
+    script.onload = function() {
+      window.bookshopLive = new window.BookshopLive();
+      window.CloudCannon = {
+        trigger: function (eventName, frontMatter) {
+            if (typeof frontMatter === 'string') frontMatter = JSON.parse(frontMatter);
+            window.bookshopLive.update({page: frontMatter});
+        }
+      }
+    }
+    head.appendChild(script);
+  }
+});
+</script>"
+    end
+  end
+end
