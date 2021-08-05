a57cc745becebba39fd7a4b0183cf3e675886fca
Liam Bigelow <liam@cloudcannon.com>
2021-08-05 21:49:38 +1200

    Releasing 2.0.0-beta.13

--- generator-plugins/jekyll/cloudcannon-jekyll-bookshop/lib/cloudcannon-jekyll-bookshop/version.rb
+++ generator-plugins/jekyll/cloudcannon-jekyll-bookshop/lib/cloudcannon-jekyll-bookshop/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CloudCannonJekyllBookshop
-  VERSION = "2.0.0.pre.beta.12"
+  VERSION = "2.0.0.pre.beta.13"
 end
--- generator-plugins/jekyll/jekyll-bookshop/lib/jekyll-bookshop/version.rb
+++ generator-plugins/jekyll/jekyll-bookshop/lib/jekyll-bookshop/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module JekyllBookshop
-  VERSION = "2.0.0.pre.beta.12"
+  VERSION = "2.0.0.pre.beta.13"
 end
