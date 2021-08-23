edc4e961863a3023ab4cec505ab28abac3ff5525
Liam Bigelow <liam@cloudcannon.com>
2021-08-23 22:15:27 +1200

    Add state management to svelte-engine

--- javascript-modules/engines/svelte-engine/lib/engine.js
+++ javascript-modules/engines/svelte-engine/lib/engine.js
@@ -10,6 +10,7 @@ export class Engine {
         this.key = 'svelte';
         this.name = options.name;
         this.files = options.files;
+        this.activeApps = {};
     }
 
     getShared(name) {
@@ -32,13 +33,33 @@ export class Engine {
     }
 
     async render(target, name, props, globals) {
+        if (!globals || typeof globals !== "object") globals = {};
+        props = { ...globals, ...props };
+
+        if (this.activeApps[target]?.name === name) {
+            return this.update(target, props);
+        }
+
         let source = this.getComponent(name);
         if (!source) {
             console.warn(`[svelte-engine] No component found for ${name}`);
             return "";
         }
-        if (!globals || typeof globals !== "object") globals = {};
-        props = { ...globals, ...props };
-        new source({target, props});
+        this.activeApps[target] = {
+            name,
+            app: new source({target, props})
+        };
+    }
+
+    update(target, props) {
+        if (!this.activeApps[target]?.app) return;
+        this.activeApps[target].app?.$$set?.(props);
+    }
+
+    destroy(target, name) {
+        if (!name || this.activeApps[target]?.name === name) {
+            this.activeApps[target].app?.$destroy?.();
+        }
+        delete this.activeApps[target];
     }
 }
