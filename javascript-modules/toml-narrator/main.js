b32f45ddadd9d75e75124366b1d634ba8cd89a07
Jan Claasen <jan@cloudcannon.com>
2021-10-12 19:19:46 +1300

    fix(toml-narrator): Remove comments to prevent broken tests
    Co-authored-by: Liam Bigelow <40188355+bglw@users.noreply.github.com>

--- javascript-modules/toml-narrator/main.js
+++ javascript-modules/toml-narrator/main.js
@@ -13,7 +13,6 @@ module.exports = {
             if (isVariableComment.test(line)) {
                 const [, comment] = extractComment.exec(line) || [];
                 const [, variable_name] = extractVariable.exec(line) || [];
-                console.log(comment)
                 if (!comment || !variable_name) return line;
                 return `${variable_name}--bookshop_comment = "${comment.replace(/"/g, '\"').trim()}"\n${line}`
             } else if (isBlockComment.test(line)) {
