876e9a47951d3a39b5b1923600cc24024cbff912
Liam Bigelow <liam@cloudcannon.com>
2021-12-03 11:58:55 +1300

    fix: check if live rendering is in a browser context

--- javascript-modules/live/lib/app/live.js
+++ javascript-modules/live/lib/app/live.js
@@ -42,7 +42,7 @@ export const getLive = (engines) => class BookshopLive {
     }
 
     async render(options = {}) {
-        const CCEditorPanelSupport = !!window.CloudCannon?.refreshInterface;
+        const CCEditorPanelSupport = typeof window !== 'undefined' && window.CloudCannon?.refreshInterface;
         options = {
             editorLinks: CCEditorPanelSupport,
             ...options
