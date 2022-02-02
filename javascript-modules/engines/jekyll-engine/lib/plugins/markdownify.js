53af90db0028224bdc7e40e87f8045483293cda9
Liam Bigelow <liam@cloudcannon.com>
2022-02-02 19:16:56 +1300

    feat(jekyll-engine): implement markdownify for jekyll live editing
    `markdownify` uses a different kramdown implementation than jekyll,
    so the output is not guaranteed to be identical.

---
+++ javascript-modules/engines/jekyll-engine/lib/plugins/markdownify.js
@@ -0,0 +1,10 @@
+import kramed from 'kramed';
+
+/**
+ * Implementation of Jekyll's jsonify filter
+ */
+export default function (Liquid) {
+    this.registerFilter('markdownify', (input) => {
+        return kramed(input);
+    });
+}
