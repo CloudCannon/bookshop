1e2d5a5f0535f843bff8ec4ff17150a4aa1e286f
Liam Bigelow <liam@cloudcannon.com>
2022-03-24 20:59:30 +1300

    build: deleting now un-references cloudcannon-structures package

--- javascript-modules/cloudcannon-structures/vendor-hoisted-pluralize.js
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env node
-
-const path = require('path');
-const fs = require('fs');
-
-fs.copyFileSync(
-    path.join(__dirname, '../node_modules/pluralize/pluralize.js'),
-    path.join(__dirname, 'vendored-pluralize.js'),
-);
