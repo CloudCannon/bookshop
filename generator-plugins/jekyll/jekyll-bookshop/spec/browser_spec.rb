f808a6411719d979fb7beb5abd8348bd1a45a0f0
Liam Bigelow <liam@cloudcannon.com>
2022-04-01 15:56:01 +1300

    test: update jekyll test for new component browser

--- generator-plugins/jekyll/jekyll-bookshop/spec/browser_spec.rb
+++ generator-plugins/jekyll/jekyll-bookshop/spec/browser_spec.rb
@@ -11,21 +11,15 @@ module JekyllBookshop
     it "should render the bookshop browser element" do
       output_file = TestHelpers.read_output_file("index.html")
 
-      target = "<div data-bookshop-browser></div>"
+      target = "<div data-bookshop-browser=\"\"></div>"
       expect(output_file).must_match %r!#{Regexp.quote(target)}!
     end
 
     it "should render the bookshop browser dependencies" do
       output_file = TestHelpers.read_output_file("index.html")
 
-      target = "<script src=\"http://localhost:1234/bookshop.js\"></script>"
+      target = "<script src=\"http://localhost:30775/bookshop.js\"></script>"
       expect(output_file).must_match %r!#{Regexp.quote(target)}!
-    end
-
-    it "should escape script tags" do
-      output_file = TestHelpers.read_output_file("index.html")
-
-      expect(output_file).wont_match %r!</script>.*</script>!
     end
 
     it "should render the initialization snippet" do
