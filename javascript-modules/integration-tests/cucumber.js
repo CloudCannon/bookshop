3e3794c5e901d0345df3cccc08deb9d3ca33cb18
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2022-12-05 11:49:33 +1300

    Add retries for failed integration tests

--- javascript-modules/integration-tests/cucumber.js
+++ javascript-modules/integration-tests/cucumber.js
@@ -5,11 +5,11 @@
 //-│           to the local update config store via            │
 //-│ sudo chown -R $USER:$(id -gn $USER) C:\Users\Liam\.config │
 //-└───────────────────────────────────────────────────────────┘
-const retries = process.platform === "win32" ? " --retry 2" : '';
+const retries = " --retry 2";
 
 // TODO: Puppeteer tests are flaky on Windows.
 const web = process.platform === "win32" ? " --tags \"not @web\"" : '';
 
-module.exports = { 
+module.exports = {
     default: `--publish-quiet --require support${retries}${web}`
 }
