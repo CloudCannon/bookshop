534095cb9afee068233fa3256d4528c0424cbcd4
Tate <tate@cloudcannon.com>
2025-01-22 11:44:26 +1300

    Remove extra exports from middleware

--- javascript-modules/engines/astro-engine/lib/modules/middleware.js
+++ javascript-modules/engines/astro-engine/lib/modules/middleware.js
@@ -17,19 +17,3 @@ export const trySerializeLocals = (value) => {
   console.warn("[Bookshop] middleware is not supported in Bookshop. Please use an editing fallback instead.");
   return '';
 };
-
-// transitions.js
-export const ClientRouter = () => {
-  console.warn("[Bookshop] view transitions are not supported in Bookshop. Please use an editing fallback instead.");
-  return null;
-};
-
-export const fade = (opts) => {
-  console.warn("[Bookshop] view transitions are not supported in Bookshop. Please use an editing fallback instead.");
-  return {};
-};
-
-export const slide = (opts) => {
-  console.warn("[Bookshop] view transitions are not supported in Bookshop. Please use an editing fallback instead.");
-  return {};
-};
