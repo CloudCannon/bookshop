7390bbf7eb1cbdd8ffe819f075ee343653355bfa
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2025-05-15 11:22:35 +1200

    Change from yarn to a pnpm workspace

--- .backstage/publish.js
+++ .backstage/publish.js
@@ -1,87 +1,87 @@
-const path = require('path');
-const fs = require('fs');
-const execSync = require('child_process').execSync;
+const path = require("path");
+const fs = require("fs");
+const execSync = require("child_process").execSync;
 
-const packages = require('../bookshop-packages.json');
+const packages = require("../bookshop-packages.json");
 const ver = process.argv[2];
 const wet = process.argv[3];
 const env = process.env;
 
-const getPrereleaseTag = ver => ver.match(/^\d+\.\d+\.\d+(?:-([a-z]+)\.\d+)$/)?.[1];
-const formatGemVersion = ver => ver.replace(/-/, '.pre.');
+const getPrereleaseTag = (ver) =>
+  ver.match(/^\d+\.\d+\.\d+(?:-([a-z]+)\.\d+)$/)?.[1];
+const formatGemVersion = (ver) => ver.replace(/-/, ".pre.");
 
 const publishNPM = async (pkgs, version) => {
-    const npmTag = getPrereleaseTag(version) ? ` --tag ${getPrereleaseTag(version)}` : ``;
-    const releases = pkgs.map(async (pkg) => {
-        return await new Promise((resolve, reject) => {
-            try {
-                const cmd = `yarn --cwd ${pkg} publish${npmTag} --non-interactive --access public`;
-                console.log(`* ${cmd}`);
-                if (wet === "seriously") execSync(cmd, { stdio: "inherit", env });
-                resolve({ pkg, version, err: null });
-            } catch (err) {
-                resolve({ pkg, err });
-            }
-        });
-    });
-    return await Promise.all(releases);
+  const npmTag = getPrereleaseTag(version)
+    ? ` --tag ${getPrereleaseTag(version)}`
+    : "";
+  const cmd = `pnpm -r publish${npmTag} --access public`;
+  console.log(`* ${cmd}`);
+  if (wet === "seriously") execSync(cmd, { stdio: "inherit", env });
 };
 
 const publishGems = async (pkgs, version) => {
-    const gemVersion = formatGemVersion(version);
-    const releases = pkgs.map(async (pkg) => {
-        return await new Promise((resolve, reject) => {
-            try {
-                const packageName = path.basename(pkg);
-                let cmd = `cd ${pkg} && gem build ${packageName}.gemspec`;
-                console.log(`* ${cmd}`);
-                if (wet === "seriously") execSync(cmd, { stdio: "inherit", env })
-                cmd = `cd ${pkg} && gem push ${packageName}-${gemVersion}.gem`;
-                console.log(`* ${cmd}`);
-                if (wet === "seriously") execSync(cmd, { stdio: "inherit", env })
-                cmd = `rm ${pkg}/*.gem`;
-                console.log(`* ${cmd}`);
-                if (wet === "seriously") execSync(cmd, { env });
-                resolve({ pkg, version: gemVersion, err: null });
-            } catch (err) {
-                resolve({ pkg, err });
-            }
-        });
+  const gemVersion = formatGemVersion(version);
+  const releases = pkgs.map(async (pkg) => {
+    return await new Promise((resolve, reject) => {
+      try {
+        const packageName = path.basename(pkg);
+        let cmd = `cd ${pkg} && gem build ${packageName}.gemspec`;
+        console.log(`* ${cmd}`);
+        if (wet === "seriously") execSync(cmd, { stdio: "inherit", env });
+        cmd = `cd ${pkg} && gem push ${packageName}-${gemVersion}.gem`;
+        console.log(`* ${cmd}`);
+        if (wet === "seriously") execSync(cmd, { stdio: "inherit", env });
+        cmd = `rm ${pkg}/*.gem`;
+        console.log(`* ${cmd}`);
+        if (wet === "seriously") execSync(cmd, { env });
+        resolve({ pkg, version: gemVersion, err: null });
+      } catch (err) {
+        resolve({ pkg, err });
+      }
     });
-    return await Promise.all(releases);
+  });
+  return await Promise.all(releases);
 };
 
 if (!ver) {
-    console.error(`No version supplied`);
-    process.exit(1);
+  console.error(`No version supplied`);
+  process.exit(1);
 }
 
 if (wet === "true") {
-    console.log("Pushing packages for real");
+  console.log("Pushing packages for real");
 } else {
-    console.log("Doing a dry run publish");
-    console.log("Pass `seriously` after version to publish for real.\n");
+  console.log("Doing a dry run publish");
+  console.log("Pass `seriously` after version to publish for real.\n");
 }
 
 const publish = async () => {
-    const npmPublishResults = await publishNPM(Object.keys(packages.npm), ver);
-    const gemPublishResults = await publishGems(Object.keys(packages.rubygems), ver);
-    const publishFailures = [...npmPublishResults, ...gemPublishResults].filter(r => r.err).map(r => r.pkg);
-    const publishSuccesses = [...npmPublishResults, ...gemPublishResults].filter(r => !r.err).map(r => `${r.version} ${r.pkg}`);
+  const npmPublishResults = await publishNPM(Object.keys(packages.npm), ver);
+  const gemPublishResults = await publishGems(
+    Object.keys(packages.rubygems),
+    ver,
+  );
+  const publishFailures = [...npmPublishResults, ...gemPublishResults]
+    .filter((r) => r.err)
+    .map((r) => r.pkg);
+  const publishSuccesses = [...npmPublishResults, ...gemPublishResults]
+    .filter((r) => !r.err)
+    .map((r) => `${r.version} ${r.pkg}`);
 
-    if (publishFailures.length) {
-        console.error(`* * Publishing failed for the following packages:`);
-        console.error(`* * ⇛ ${publishFailures.join('\n* * ⇛ ')}`);
-        console.error(`* * The following packages __have__ been published:`);
-        console.error(`* * ⇛ ${publishSuccesses.join('\n* * ⇛ ')}`);
-        if (publishSuccesses.length) {
-            // Since publishing partly succeeded we want to continue to update the git info
-            // (so that we can re-run this publish to try mop up missing packages)
-            process.exit(0);
-        } else {
-            process.exit(1);
-        }
+  if (publishFailures.length) {
+    console.error(`* * Publishing failed for the following packages:`);
+    console.error(`* * ⇛ ${publishFailures.join("\n* * ⇛ ")}`);
+    console.error(`* * The following packages __have__ been published:`);
+    console.error(`* * ⇛ ${publishSuccesses.join("\n* * ⇛ ")}`);
+    if (publishSuccesses.length) {
+      // Since publishing partly succeeded we want to continue to update the git info
+      // (so that we can re-run this publish to try mop up missing packages)
+      process.exit(0);
+    } else {
+      process.exit(1);
     }
-}
+  }
+};
 
 publish();
--- .backstage/version.js
+++ .backstage/version.js
@@ -1,58 +1,69 @@
-const path = require('path');
-const fs = require('fs');
-const execSync = require('child_process').execSync;
+const path = require("path");
+const fs = require("fs");
+const execSync = require("child_process").execSync;
 
-const packages = require('../bookshop-packages.json');
+const packages = require("../bookshop-packages.json");
 const ver = process.argv[2];
 const env = process.env;
 
 const versionNpm = (pkgs, version) => {
-    pkgs.forEach(pkg => {
-        const cmd = `npm --prefix ${pkg} version ${version} --allow-same-version --no-git-tag-version --no-commit-hooks`;
-        console.log(`* ${cmd}`)
-        const npmBump = execSync(cmd, { env });
-        if (npmBump.stderr) {
-            console.error(`yarn version bump failed:\n${npmBump.stderr}`);
-            process.exit(1);
-        }
-    });
-    const cmd = `cd javascript-modules && yarn up '@bookshop/*@${version}'`;
-    console.log(`* ${cmd}`)
-    execSync(cmd, { env });
+  pkgs.forEach((pkg) => {
+    const cmd = `npm --prefix ${pkg} version ${version} --allow-same-version --no-git-tag-version --no-commit-hooks`;
+    console.log(`* ${cmd}`);
+    const npmBump = execSync(cmd, { env });
+    if (npmBump.stderr) {
+      console.error(`version bump failed:\n${npmBump.stderr}`);
+      process.exit(1);
+    }
+  });
 };
 
-const formatGemVersion = (ver) => ver.replace(/-/, '.pre.');
+const formatGemVersion = (ver) => ver.replace(/-/, ".pre.");
 const versionGems = (gems, version) => {
-    gems.forEach(gem => {
-        const packageName = path.basename(gem);
-        const packageVersionFile = path.join(__dirname, '../', gem, `lib/${packageName}/version.rb`);
-        let versionFileContents = fs.readFileSync(packageVersionFile, 'utf8');
-        if (!/VERSION/.test(versionFileContents)) {
-            console.error(box(`${packageName} version.rb file does not contain a VERSION constant.`));
-            process.exit(1);
-        }
+  gems.forEach((gem) => {
+    const packageName = path.basename(gem);
+    const packageVersionFile = path.join(
+      __dirname,
+      "../",
+      gem,
+      `lib/${packageName}/version.rb`,
+    );
+    let versionFileContents = fs.readFileSync(packageVersionFile, "utf8");
+    if (!/VERSION/.test(versionFileContents)) {
+      console.error(
+        box(
+          `${packageName} version.rb file does not contain a VERSION constant.`,
+        ),
+      );
+      process.exit(1);
+    }
 
-        versionFileContents = versionFileContents
-            .replace(/VERSION =.*$/gm, `VERSION = "${formatGemVersion(version)}"`);
-        fs.writeFileSync(packageVersionFile, versionFileContents);
-        console.log(`* Updating ruby ${packageVersionFile}`);
-    });
+    versionFileContents = versionFileContents.replace(
+      /VERSION =.*$/gm,
+      `VERSION = "${formatGemVersion(version)}"`,
+    );
+    fs.writeFileSync(packageVersionFile, versionFileContents);
+    console.log(`* Updating ruby ${packageVersionFile}`);
+  });
 };
 
 const versionStrings = (files, version) => {
-    Object.entries(files).forEach(([file, { regex, replacement }]) => {
-        let fileContents = fs.readFileSync(path.join(__dirname, '../', file), 'utf8');
-        let r = new RegExp(regex, 'g');
-        let new_text = replacement.replace(/NEW_VERSION/g, version);
-        fileContents = fileContents.replace(r, new_text);
-        fs.writeFileSync(file, fileContents);
-        console.log(`* Updating version string in ${file}`);
-    })
-}
+  Object.entries(files).forEach(([file, { regex, replacement }]) => {
+    let fileContents = fs.readFileSync(
+      path.join(__dirname, "../", file),
+      "utf8",
+    );
+    let r = new RegExp(regex, "g");
+    let new_text = replacement.replace(/NEW_VERSION/g, version);
+    fileContents = fileContents.replace(r, new_text);
+    fs.writeFileSync(file, fileContents);
+    console.log(`* Updating version string in ${file}`);
+  });
+};
 
 if (!ver) {
-    console.error(`No version supplied`);
-    process.exit(1);
+  console.error(`No version supplied`);
+  process.exit(1);
 }
 
 versionNpm(Object.keys(packages.npm), ver);
@@ -60,5 +71,8 @@ versionGems(Object.keys(packages.rubygems), ver);
 versionStrings(packages.versionStrings, ver);
 
 packages.version = ver;
-fs.writeFileSync(path.join(__dirname, '../bookshop-packages.json'), JSON.stringify(packages, null, 2));
+fs.writeFileSync(
+  path.join(__dirname, "../bookshop-packages.json"),
+  JSON.stringify(packages, null, 2),
+);
 console.log(`* Updated version in bookshop-packages.json`);
